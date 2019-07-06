fb28f3301a014f12147d8c2f55ed63058ee02718
miladHakimi <miladhakimi33@gmail.com>
2019-07-06 08:02:48 +0430

    filter added to conv input.

--- CustomConv2d.cc
+++ CustomConv2d.cc
@@ -1,19 +1,24 @@
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/framework/common_shape_fns.h"
 #include <string>
 
 using namespace tensorflow;
 
 REGISTER_OP("CustomConv2d")
-    .Input("
-        input: float32[][],
-        filter: float32[][], 
-        strides: String, 
-        Padding: String,
-        name=String,
-        ");
-    .Output("conved: float32[][]")
+    .Input("input: float32")
+    .Input("filter: float32")
+    .Output("conved: float32")
+    // .SetShapeFn(::tensorflow::shape_inference::DepthwiseConv2DNativeShape);
     .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
-      c->set_output(0, c->input(0));
-      return Status::OK();
+        ::tensorflow::shape_inference::ShapeManager a;
+        ::tensorflow::shape_inference::ShapeHandle b;
+        // b = a.MakeShape({1, 1});
+
+        // TF_RETURN_IF_ERROR(c->MakeShapeFromShapeTensor(1, &n));
+        ::tensorflow::shape_inference::ShapeHandle out;
+        // TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 1, &out));
+        // TF_RETURN_IF_ERROR(c->Concatenate(n, c->input(0), &out));
+        c->set_output(0, out);
+        return Status::OK();
     });
