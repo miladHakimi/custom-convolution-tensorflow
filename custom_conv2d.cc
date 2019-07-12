3fc62c1e702d87db205e5787203d9e785c347b92
miladHakimi <miladhakimi33@gmail.com>
2019-07-12 23:35:13 +0430

    Results are being calculated and set to the output.

--- custom_conv2d.cc
+++ custom_conv2d.cc
@@ -2,11 +2,6 @@
 
 using namespace tensorflow;
 
-#define IMG_W 2
-#define IMG_H 2
-#define FIL_W 2
-#define FIL_H 2
-
 class CustomConv2dOp : public OpKernel {
     public:
     explicit CustomConv2dOp(OpKernelConstruction* context) : OpKernel(context) {}
@@ -17,16 +12,23 @@ class CustomConv2dOp : public OpKernel {
         const Tensor& input_tensor2 = context->input(1);
         
         tensorflow::TensorShape ts(input_tensor1.shape());
+        int FIL_H = input_tensor2.shape().dim_size(0);
+        int FIL_W =input_tensor2.shape().dim_size(1);
+        int IMG_H = input_tensor1.shape().dim_size(0);
+        int IMG_W = input_tensor1.shape().dim_size(1);
+
         int width = (input_tensor1.shape().dim_size(1)-input_tensor2.shape().dim_size(1)+1);
         int height = (input_tensor1.shape().dim_size(0)-input_tensor2.shape().dim_size(0)+1);
+        
         ts.set_dim(0, height);
         ts.set_dim(1, width);
 
         auto input = input_tensor1.flat<float>();
+        auto in1 = input_tensor2.flat<float>();
         float partial_conv;
         float conv_res[height][width];
-        int filter[FIL_H][FIL_W] = {{1, 0}, {1, -1}};
-        float img[IMG_H][IMG_W] = {{1, 0}, {1, -1}};
+        int filter[2][2] = {{1, 0}, {1, -1}};
+        float img[2][2] = {{1, 0}, {1, -1}};
 
         // Create an output tensor
         Tensor* output_tensor = NULL;
@@ -34,19 +36,18 @@ class CustomConv2dOp : public OpKernel {
         auto output_flat = output_tensor->flat<float>();
         
         // convolution body
-        
+    
         for (int i = 0; i < height; i++)
             for (int j = 0; j < width; j++){
                 partial_conv = 0;
                 for (int k = 0; k < FIL_H; k++)
                     for (int l = 0; l < FIL_W; l++)
-                        partial_conv += filter[k][l] * img[i+k][l+j];
+                        partial_conv += in1(k*FIL_W+l) * input((i+k)*IMG_W+l+j);
                 
                 conv_res[i][j] = partial_conv;
             }
-        for (int i = 0; i <(height) * (width) ; i++) {
-            output_flat(i) = conv_res[i/(height)][i%(width)];
-        }
+        for (int i = 0; i <(height) * (width) ; i++)
+            output_flat(i) = conv_res[i/(width)][i%(width)];
 
     }
 };
