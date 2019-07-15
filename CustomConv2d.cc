d7748d7823f39a60c320419ca25a37a0549b1a16
miladHakimi <miladhakimi33@gmail.com>
2019-07-15 17:24:34 +0430

    some minor improvements in the function

--- CustomConv2d.cc
+++ CustomConv2d.cc
@@ -1,24 +1,25 @@
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/shape_inference.h"
 #include "tensorflow/core/framework/common_shape_fns.h"
-#include <string>
+#include "tensorflow/core/util/padding.h"
+#include <vector>
 
 using namespace tensorflow;
 
 REGISTER_OP("CustomConv2d")
+    // .Attr("strides: list(int) = []")
+    // .Attr("padding: string")
     .Input("input: float32")
     .Input("filter: float32")
     .Output("conved: float32")
-    // .SetShapeFn(::tensorflow::shape_inference::DepthwiseConv2DNativeShape);
     .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
-        ::tensorflow::shape_inference::ShapeManager a;
-        ::tensorflow::shape_inference::ShapeHandle b;
-        // b = a.MakeShape({1, 1});
-
-        // TF_RETURN_IF_ERROR(c->MakeShapeFromShapeTensor(1, &n));
-        ::tensorflow::shape_inference::ShapeHandle out;
-        // TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 1, &out));
-        // TF_RETURN_IF_ERROR(c->Concatenate(n, c->input(0), &out));
-        c->set_output(0, out);
-        return Status::OK();
+      c->set_output(0, c->input(0));
+      return Status::OK();
     });
+    // .SetShapeFn(::tensorflow::shape_inference::DepthwiseConv2DNativeShape);
+    // .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+    //     ::tensorflow::shape_inference::DimensionOrConstant doc(1);
+    //     return ConcatV2Shape(c);
+    //     // c->set_output(0, c->MakeShape({doc}));
+    //     // return Status::OK();
+    // });
