5da227680118215b7b86ac4aa2161317426c31c0
miladHakimi <miladhakimi33@gmail.com>
2019-07-27 21:28:18 +0430

    output shape added

--- CustomConv2d.cc
+++ CustomConv2d.cc
@@ -14,6 +14,21 @@ REGISTER_OP("CustomConv2d")
     .Input("padding: string")
     .Output("conved: float32")
     .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
-      c->set_output(0, c->input(0));
-      return Status::OK();
+    	::tensorflow::shape_inference::ShapeHandle input_shape;
+		TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 4, &input_shape));
+		::tensorflow::shape_inference::ShapeHandle filter_shape;
+		TF_RETURN_IF_ERROR(c->WithRank(c->input(1), 4, &filter_shape));
+		
+		::tensorflow::shape_inference::DimensionHandle batch_size_dim = c->Dim(input_shape, 0);
+		::tensorflow::shape_inference::DimensionHandle in_rows_dim = c->Dim(input_shape, 1);
+		::tensorflow::shape_inference::DimensionHandle in_cols_dim = c->Dim(input_shape, 2);
+		::tensorflow::shape_inference::DimensionHandle output_depth_dim = c->Dim(filter_shape, 3);
+
+		::tensorflow::shape_inference::ShapeHandle output_shape;
+  		TensorFormat data_format;
+      	output_shape = c->MakeShape({batch_size_dim, in_rows_dim,
+                                 in_cols_dim, output_depth_dim});
+		c->set_output(0, output_shape);
+		return Status::OK();
     });
+   
