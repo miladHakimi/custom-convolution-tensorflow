554007e2530fb733153f09b1347262a15f46c14c
miladHakimi <miladhakimi33@gmail.com>
2019-07-28 17:58:39 +0430

    shape format changed

--- custom_conv2d.cc
+++ custom_conv2d.cc
@@ -20,7 +20,6 @@ class CustomConv2dOp : public OpKernel {
         int FIL_W =input_tensor2.shape().dim_size(1);
         int IMG_H = input_tensor1.shape().dim_size(1);
         int IMG_W = input_tensor1.shape().dim_size(2);
-
         int width , height, count, depth;
 
         if(padding(0) == "SAME"){
@@ -32,7 +31,7 @@ class CustomConv2dOp : public OpKernel {
             height = ceil((input_tensor1.shape().dim_size(1)-input_tensor2.shape().dim_size(0)+1)/strides(0));
         }
         count = input_tensor1.shape().dim_size(0);
-        depth = input_tensor2.shape().dim_size(3);
+        depth = input_tensor2.shape().dim_size(0);
         tensorflow::TensorShape ts(input_tensor1.shape());
         ts.set_dim(0, count);
         ts.set_dim(1, height);
@@ -49,20 +48,23 @@ class CustomConv2dOp : public OpKernel {
         OP_REQUIRES_OK(context, context->allocate_output(0, ts,&output_tensor));
         auto output_flat = output_tensor->flat<float>();
         // convolution body
+        int flag = 0;
         for (int m = 0; m < count; m++){
             for (int n = 0; n < depth; n++){
                 for (int i = 0; i < height; i+=1){
                     for (int j = 0; j < width; j+=1){
                         partial_conv = 0;
+                        flag = 0;
                         for (int k = 0; k < FIL_H; k+=1)
                             if(i*strides(0)+k < IMG_H)
                                 for (int l = 0; l < FIL_W; l+=1)
-                                    if(j*strides(0)+l < IMG_W)
+                                    if(j*strides(0)+l < IMG_W){
                                         partial_conv += filter(n*FIL_H*FIL_W+k*FIL_W+l) * img(m*(IMG_H*IMG_W)+ (i*strides(0)+k)*IMG_W+l+j*strides(1));
+                                    }
                                     else
                                         break;           
                             else
-                                break;                                 
+                                break;                                                         
                         output_flat(m*height*width*depth+n*height*width+i*width+j) = partial_conv;
                     }
                 }
