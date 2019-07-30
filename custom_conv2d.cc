143240dc37ba289f796aa4fbf34bac928c8db5e2
miladHakimi <miladhakimi33@gmail.com>
2019-07-30 21:50:06 +0430

    padding problem fixed

--- custom_conv2d.cc
+++ custom_conv2d.cc
@@ -14,7 +14,7 @@ class CustomConv2dOp : public OpKernel {
         const Tensor& input_tensor4 = context->input(3);
         
         auto padding = input_tensor4.flat<string>();
-        auto strides = input_tensor3.flat<float>();
+        auto strides = input_tensor3.flat<int>();
         
         int FIL_H = input_tensor2.shape().dim_size(0);
         int FIL_W =input_tensor2.shape().dim_size(1);
@@ -30,8 +30,16 @@ class CustomConv2dOp : public OpKernel {
             width = ceil((input_tensor1.shape().dim_size(2)-input_tensor2.shape().dim_size(1)+1)/strides(1));
             height = ceil((input_tensor1.shape().dim_size(1)-input_tensor2.shape().dim_size(0)+1)/strides(0));
         }
+
+        int pad_along_height = std::max((height-1)*strides(1)+FIL_H-IMG_H, 0);
+        int pad_along_width = std::max((width-1)*strides(2)+FIL_W-IMG_W, 0);
+        int pad_top = pad_along_height/2;
+        int pad_bottom = pad_along_height - pad_top;
+        int pad_left = pad_along_width/2;
+        int pad_right = pad_along_width - pad_left;
+
         count = input_tensor1.shape().dim_size(0);
-        depth = input_tensor2.shape().dim_size(0);
+        depth = input_tensor2.shape().dim_size(3);
         tensorflow::TensorShape ts(input_tensor1.shape());
         ts.set_dim(0, count);
         ts.set_dim(1, height);
@@ -40,32 +48,34 @@ class CustomConv2dOp : public OpKernel {
 
         auto img = input_tensor1.flat<float>();
         auto filter = input_tensor2.flat<float>();
-        float partial_conv;
-        float conv_res[height][width];
-        float conv_ans[count][height][width];
+        long double partial_conv;
+
         // Create an output tensor
         Tensor* output_tensor = NULL;
         OP_REQUIRES_OK(context, context->allocate_output(0, ts,&output_tensor));
         auto output_flat = output_tensor->flat<float>();
         // convolution body
-        int flag = 0;
+        int comp_img_height = 0;
+        int comp_img_width = 0;
         for (int m = 0; m < count; m++){
             for (int n = 0; n < depth; n++){
                 for (int i = 0; i < height; i+=1){
                     for (int j = 0; j < width; j+=1){
                         partial_conv = 0;
-                        flag = 0;
-                        for (int k = 0; k < FIL_H; k+=1)
-                            if(i*strides(0)+k < IMG_H)
-                                for (int l = 0; l < FIL_W; l+=1)
-                                    if(j*strides(0)+l < IMG_W){
-                                        partial_conv += filter(n*FIL_H*FIL_W+k*FIL_W+l) * img(m*(IMG_H*IMG_W)+ (i*strides(0)+k)*IMG_W+l+j*strides(1));
-                                    }
+                        for (int k = 0; k < FIL_H; k+=1){
+                            comp_img_height = i * strides(0) + k - pad_top;
+                            if(comp_img_height >= 0 && comp_img_height < IMG_H + pad_bottom)
+                                for (int l = 0; l < FIL_W; l+=1){
+                                    comp_img_width = j * strides(0) + l - pad_left;
+                                    if(comp_img_width >= 0 && comp_img_width < IMG_W + pad_right)
+                                        partial_conv += (long double)filter((k * FIL_W + l) * depth + n) * (long double)img( m * (IMG_H*IMG_W) + comp_img_height * IMG_W + comp_img_width);
                                     else
-                                        break;           
+                                        break;       
+                                }    
                             else
-                                break;                                                         
-                        output_flat(m*height*width*depth+n*height*width+i*width+j) = partial_conv;
+                                break;  
+                        }
+                        output_flat(m*height*width*depth+(i*width+j)*depth+n) = partial_conv;
                     }
                 }
             }
