7f56d0e54e5d5075d940b55553bb22889fb8a6e4
miladHakimi <miladhakimi33@gmail.com>
2019-07-30 21:50:37 +0430

    some minor improvements in type of inputs

--- CustomConv2d.cc
+++ CustomConv2d.cc
@@ -10,7 +10,7 @@ using namespace tensorflow;
 REGISTER_OP("CustomConv2d")
     .Input("input: float32")
     .Input("filter: float32")
-    .Input("strides: float32")
+    .Input("strides: int32")
     .Input("padding: string")
     .Output("conved: float32")
     .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
